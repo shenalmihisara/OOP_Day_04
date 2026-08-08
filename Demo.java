443a9f2e5de28f63df58960d40c66be5014bcd97
shenalmihisara <shenalmihisara51@gmail.com>
2026-08-08 17:42:21 +0530

    Implement stack class with dynamic array and search functionality

--- Demo.java
+++ Demo.java
@@ -60,7 +60,7 @@ class Demo{
 }*/
 
 
-class stack{
+/*class stack{
 	private int[] dataArray; 
 	stack(){
 		dataArray=new int[0];
@@ -88,6 +88,15 @@ class stack{
 		}
 	}
 	
+	public int search(int data){
+		for(int i=0; i<dataArray.length; i++){
+			if(dataArray[i]==data){
+				return i;
+			}
+		}
+		return-1;
+	}
+	
 	public void display(){
 		System.out.print("[");
 		for(int i=0; i<dataArray.length; i++){
@@ -113,5 +122,73 @@ class Demo{
 		s1.display();
 		s1.pop();
 		s1.display();
+		
+		System.out.println("index of 40 : "+s1.search(10));
+	}
+}*/
+
+
+class stack{
+	private int[] dataArray; 
+	private int nextIndex;
+	
+	stack(int size){
+		dataArray=new int[size];
+		nextIndex=0;
+	}
+	
+	public void extendsArray(){
+		
+	}
+	
+	private boolean isFull(){
+		return nextIndex>=dataArray.length;
+	}
+	
+	public void push(int data){
+		if(isFull()){
+			extendsArray();
+		}
+		dataArray[nextIndex++]=data;
+	}
+	
+	public void pop(){
+		if(isEmpty()){
+			nextIndex--;
+		}
+	}
+	
+	public int search(int data){
+		return -1;
+	}
+	
+	public void display(){
+		System.out.print("[");
+		for(int i=nextIndex-1; i>=0; i--){
+			System.out.print(dataArray[i]+",");
+		}
+		System.out.println(isEmpty()?"empty":"\b]");
+	}
+	
+	public boolean isEmpty(){
+		return nextIndex<=0;
+	}
+}
+
+class Demo{
+	public static void main(String[] args){
+		stack s1=new stack(100);
+		
+		s1.push(10);
+		s1.push(20);
+		s1.push(30);
+		s1.push(40);
+		s1.push(50);
+		
+		s1.display();
+		s1.pop();
+		s1.display();
+		
+		System.out.println("index of 40 : "+s1.search(10));
 	}
 }
